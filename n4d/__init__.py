2b92a3425327ec23bea871daafc650c52e4a3616
Lt-Henry <quiqueiii@gmail.com>
2020-10-15 07:04:53 +0200

    wip

--- n4d/__init__.py
+++ n4d/__init__.py
@@ -17,8 +17,12 @@ class UnknownClassError(Exception):
         super().__init__(self,"Class %s not found"%name)
 
 class UnknownMethodError(Exception):
-    def __init__(self,name):
-        super().__init__(self,"Method %s not found"%name)
+    def __init__(self,name,method):
+        super().__init__(self,"Method %s::%s() not found"%(name,method))
+
+class UserNotAllowedError(Exception):
+    def __init__(self,user,name,method):
+        super().__init__(self,"%s not allowed to %s::%s()"%(user,name,method))
 
 class AuthenticationError(Exception):
     def __init__(self,name):
@@ -32,6 +36,25 @@ class InvalidServerResponseError(Exception):
     def __init__(self,server):
         super().__init__(self,"Invalid response from server %s "%server)
 
+AUTH_ANONYMOUS=1
+AUTH_PASSWORD=2
+AUTH_KEY=3
+
+class Credential:
+    def __init__(self,user=None,password=None,key=None):
+
+        self.auth_type=AUTH_ANONYMOUS
+
+        if (user and password):
+            self.auth_type=AUTH_PASSWORD
+            
+        if (key):
+            self.auth_type=AUTH_KEY
+        
+        self.user=user
+        self.password=password
+        self.key=key
+    
 class Proxy:
     def __init__(self,client,name):
         self.client=client
@@ -43,7 +66,27 @@ class Proxy:
         
         response = server.do()
         
-        return response
+        if (("msg" in response) and ("status" in response) and ("return" in response)):
+            status=response["status"]
+            
+            if (status==UNKNOWN_CLASS):
+                raise UnknownClassError(self.name)
+            
+            if (status==UNKNOWN_METHOD):
+                raise UnknownMethodError(self.name,self.method)
+            
+            if (status==USER_NOT_ALLOWED):
+                pass
+            
+            if (status==AUTHENTICATION_ERROR):
+                pass
+            
+            if (status==INVALID_RESPONSE):
+                raise InvalidMethodResponseError(self.name,self.method)
+        else:
+            raise InvalidServerResponseError()
+        
+        return response["return"]
     
     def __getattr__(self,method):
         self.method=method
