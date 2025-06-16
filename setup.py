d70a266ab26a003907eb209933829c20879c98f2
Raul Rodrigo Segura <raurodse@gmail.com>
2025-06-16 11:24:21 +0200

    add old setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(  name             = "n4d client",
+        version          = "1.0",
+        author           = "Enrique Medina Gremaldos",
+        author_email     = "quiqueiii@gmail.com",
+        url              = "https://github.com/edupals/python3-n4d-client",
+        packages         = ['n4d.client'],
+        package_dir      = {'n4d.client':'src'},
+        data_files       = [('/usr/bin/',['n4d-client'])]
+     )
+
+
